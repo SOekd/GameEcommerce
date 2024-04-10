9924e390a669b2b38845c2fac954d4bdab709d21
SOekd <augustorech.contato@gmail.com>
2024-04-10 19:58:49 -0300

    first commit

---
+++ gameecommerce-backend/src/main/java/com/gameecommerce/backend/GameecommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gameecommerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameecommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameecommerceBackendApplication.class, args);
+    }
+
+}
---
+++ gameecommerce-backend/src/test/java/com/gameecommerce/backend/GameecommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameecommerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameecommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
